05fbd98b8413b0497cdbdd4f2800e2fcd5178ce5
Ely Haughie <elycin2@gmail.com>
2019-02-19 19:46:03 -0500

    Updating to contain dynamic finding of python3

--- create-service.sh
+++ create-service.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 SERVICE="pihole-stat-rainbow-hat"
+PYTHONPATH=$(which python3)
 
 echo "Installation directory will be $PWD"
 
-echo "Editing service file to contain current working directory ..."
+echo "Editing service file ..."
 sed -i "s|{directory}|$PWD|g" $PWD/$SERVICE.service
+sed -i "s|{python3}|$PYTHONPATH|g" $PWD/$SERVICE.service
 
 echo "Copying service file into /etc/systemd/system/ ..."
 sudo cp $PWD/$SERVICE.service  /etc/systemd/system/$SERVICE.service
