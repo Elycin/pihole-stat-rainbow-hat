749b7b0ab5791d8567d9e8fb0bd7651df430c003
Ely Haughie <elycin2@gmail.com>
2019-02-21 07:11:02 -0500

    Copy-than-edit approach

--- create-service.sh
+++ create-service.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
+$SYSTEMDPATH="/etc/systemd/system"
 SERVICE="pihole-stat-rainbow-hat"
 PYTHONPATH=$(which python3)
 
 echo "Installation directory will be $PWD"
 
-# Create backup of service
-cp $PWD/$SERVICE.service $PWD/$SERVICE.service.bak
+echo "Copying service file into $SYSTEMDPATH ..."
+sudo cp $PWD/$SERVICE.service  $SYSTEMDPATH/$SERVICE.service
 
 echo "Editing service file ..."
-sed -i "s|{directory}|$PWD|g" $PWD/$SERVICE.service
-sed -i "s|{python3}|$PYTHONPATH|g" $PWD/$SERVICE.service
-
-echo "Copying service file into /etc/systemd/system/ ..."
-sudo cp $PWD/$SERVICE.service  /etc/systemd/system/$SERVICE.service
-
-# Restore the original
-rm -rf $PWD/$SERVICE.service
-mv $PWD/$SERVICE.service.bak $PWD/$SERVICE.service
+sed -i "s|{directory}|$PWD|g" $SYSTEMDPATH/$SERVICE.service
+sed -i "s|{python3}|$PYTHONPATH|g" $SYSTEMDPATH/$SERVICE.service
 
 echo "Restarting Systemd daemon..."
 sudo systemctl daemon-reload
